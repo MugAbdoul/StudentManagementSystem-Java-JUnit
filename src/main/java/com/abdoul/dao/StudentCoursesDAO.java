4fe1bc3d0cc4d568dc36fd19be4e5475911ae894
MugAbdoul <abdoullatif5027@gmail.com>
2024-07-10 14:37:51 +0200

    Add more methods in studentCourses

--- src/main/java/com/abdoul/dao/StudentCoursesDAO.java
+++ src/main/java/com/abdoul/dao/StudentCoursesDAO.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import com.abdoul.models.Course;
 import com.abdoul.models.StudentCourses;
@@ -46,35 +47,31 @@ public class StudentCoursesDAO {
         }
     }
 
-    public void updateStudentCourse(StudentCourses studentCourse) {
-        Transaction transaction = null;
+    public int getTotalMarks(StudentRegistration studentRegistration) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.update(studentCourse);
-            transaction.commit();
+            String hql = "SELECT SUM(marksIncourse) FROM StudentCourses WHERE studentRegistration = :studentRegistration";
+            Query<Integer> query = session.createQuery(hql, Integer.class);
+            query.setParameter("studentRegistration", studentRegistration);
+            return query.uniqueResult();
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             e.printStackTrace();
+            return 0;
         }
     }
 
-    public void deleteStudentCourse(UUID id) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            StudentCourses studentCourse = session.get(StudentCourses.class, id);
-            if (studentCourse != null) {
-                session.delete(studentCourse);
-                System.out.println("Student course is deleted");
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
+    public double normalizeMarksTo20(int totalMarks) {
+        return (totalMarks / 100.0) * 20.0;
+    }
+
+    public String gradeStudent(double normalizedMarks) {
+        if (normalizedMarks >= 16) {
+            return "High Distinction";
+        } else if (normalizedMarks >= 12 && normalizedMarks < 16) {
+            return "Lower Distinction";
+        } else if (normalizedMarks >= 10 && normalizedMarks < 12) {
+            return "Pass";
+        } else {
+            return "Expel";
         }
     }
 }
